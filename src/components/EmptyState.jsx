1fe66037b47e7cb99a5615b146feff659eac11fb
baek-32 <kyumin645844@gmail.com>
2026-09-16 12:39:43 +0900

    fix: 빈 상태 메모 추가 버튼 동작 연결

--- src/components/EmptyState.jsx
+++ src/components/EmptyState.jsx
@@ -1,13 +1,14 @@
 import addMemoIcon from "../assets/add-memo.svg";
 import IconButton from "./IconButton.jsx";
 
-function EmptyState() {
+function EmptyState({ onAddMemo }) {
   return (
     <section className="flex h-177.5 w-full flex-col items-center justify-center gap-2.5 rounded-3xl border-2 border-dashed border-blue-02">
       <IconButton
         icon={addMemoIcon}
         label="메모 추가"
         className="h-30 w-30 bg-blue-02 hover:bg-blue-03 [&_img]:h-9.75 [&_img]:w-9.75"
+        onClick={onAddMemo}
       />
 
       <p className="text-heading-medium font-semibold text-blue-02">
